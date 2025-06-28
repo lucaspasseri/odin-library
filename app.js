7ac81df315eb6ebf8ab994cefd4265609f7a4908
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-06-28 04:27:00 -0300

    Create a createDisplay factory to manipulate DOM

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-class Book {
-	id;
-	title;
-	author;
-	pages;
-
-	constructor(title = "(empty)", author = "(empty)", pages = 0) {
-		this.id = crypto.randomUUID();
-		this.title = title;
-		this.author = author;
-		this.pages = pages;
-	}
-}
-
 const myLibrary = (function () {
 	const myLibraryArr = [];
+	const display = createDisplay();
+
+	function init() {
+		display.init();
+		display.render();
+	}
 
 	function getLibrary() {
 		return [...myLibraryArr];
@@ -35,13 +27,74 @@ const myLibrary = (function () {
 	}
 
 	return {
+		init,
 		getLibrary,
 		addBook,
 		removeBookById,
 	};
 })();
-const b1 = new Book("era uma vez", "artesão", 201);
-const b2 = new Book("Aqueles passos", "artesão", 123);
-myLibrary.addBook(b1);
-myLibrary.addBook(b2);
-console.log(myLibrary.getLibrary());
+
+class Book {
+	id;
+	title;
+	author;
+	pages;
+
+	constructor(title = "(empty)", author = "(empty)", pages = 0) {
+		this.id = crypto.randomUUID();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+	}
+}
+
+function createDisplay() {
+	const bookListContainer = document.querySelector(".book-list-container");
+	const openDialogBtn = document.querySelector(".open-dialog-btn");
+	const dialog = document.querySelector(".dialog");
+	const form = document.querySelector(".form");
+
+	function init() {
+		openDialogBtn.addEventListener("click", () => {
+			dialog.showModal();
+		});
+
+		form.addEventListener("submit", e => {
+			const form = e.target;
+
+			const formData = new FormData(form);
+			const title = formData.get("title") || undefined;
+			const author = formData.get("author") || undefined;
+			const pages = formData.get("pages") || undefined;
+
+			const newBook = new Book(title, author, pages);
+
+			myLibrary.addBook(newBook);
+			render();
+		});
+	}
+
+	function render() {
+		bookListContainer.innerHTML = "";
+
+		const ul = document.createElement("ul");
+
+		myLibrary.getLibrary().forEach(book => {
+			const li = document.createElement("li");
+			li.textContent = book.title;
+			li.setAttribute("data-id", book.id);
+			li.classList.add("card");
+
+			ul.appendChild(li);
+		});
+
+		bookListContainer.appendChild(ul);
+	}
+
+	return {
+		init,
+		render,
+	};
+}
+
+myLibrary.init();
