286ed98971e0731f88d950b20700fa42214ef5a7
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-07-02 14:55:57 -0300

    Improve left and right scroll buttons
    Improve layout and styles of the book list left
    and right scroll buttons

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ function createDisplay() {
 			cardAuthor.textContent = `Author: ${book.author}`;
 
 			const cardPages = document.createElement("p");
-			cardPages.textContent = `#: ${book.pages} pages`;
+			cardPages.textContent = `# of pages: ${book.pages}`;
 
 			const cardInfo = document.createElement("div");
 			cardInfo.appendChild(cardAuthor);
@@ -197,13 +197,17 @@ function createDisplay() {
 				render();
 			});
 
-			const trashCanImg = document.createElement("img");
-			trashCanImg.setAttribute("src", "./imgs/trash-can-outline.svg");
-			trashCanImg.setAttribute("alt", "Trash can");
-			trashCanImg.setAttribute("width", "30px");
-			trashCanImg.setAttribute("height", "30px");
+			const trashIcon = `
+				<svg xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 0 24 24" 
+						fill="currentColor" 
+						width="24" 
+						height="24">
+					<path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+				</svg>
+			`;
 
-			deleteButton.appendChild(trashCanImg);
+			deleteButton.innerHTML = trashIcon;
 
 			const deleteButtonLabel = document.createElement("label");
 
